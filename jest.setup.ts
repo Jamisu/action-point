2a4e9e11fb9d59511e29bc843aaae31b745f6f77
ActionPoint <a.pocentek@gmail.com>
2026-03-03 16:25:44 +0100

    globalised jest visibility for TypeScript

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom'
+
+// Make Jest globals available to TypeScript
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R
+    }
+  }
+}
